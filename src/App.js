2cd862943e9932389f972c08942420886417a4b6
Stiofáin <stiofainf@gmail.com>
2021-03-22 17:56:56 +0000

    Small reorg

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import React, { useState } from 'react'
 import Wafermap from './components/Wafermap.js';
 
 function App() {
-  const [wafer, setWafer] = useState(0)
+  const [waferIdx, setWaferIdx] = useState(0)
 
   var data = require('./waferdata.json');
   var dimensions = {"width": 400, "height": 400}
 
   function nextWafer() {
-    wafer === (data.length - 1) ?
-      setWafer(0) : setWafer(wafer + 1)
+    waferIdx === (data.length - 1) ?
+      setWaferIdx(0) : setWaferIdx(waferIdx + 1)
   }
 
   return (
     <div className="App">
       <h3>Wafermap</h3>
       <Wafermap
-       points={data[wafer].points}
+       points={data[waferIdx].points}
        configuration={dimensions}
       />
       <button onClick={nextWafer}>Next Wafer</button>
--- src/components/d3Wafermap.js
+++ src/components/d3Wafermap.js
@@ -6,8 +6,22 @@ d3Wafermap.create = (el, data, configuration) => {
     .attr("width", configuration.width)
     .attr("height", configuration.height)
 
-  svg.append("circle").attr("id", "d3-outer-circ")
-  svg.append("circle").attr("id", "d3-inner-circ")
+  svg.append("circle")
+    .style("fill", "gray")
+    .style("stroke", "black")
+    .style("stroke-width", 0.025)
+    .attr("id", "d3-outer-circ")
+    .attr("cx", 0.5)
+    .attr("cy", 0.5);
+
+  svg.append("circle")
+    .style("fill", "gray")
+    .style("stroke", "black")
+    .style("stroke-width", 0.025)
+    .attr("id", "d3-inner-circ")
+    .attr("cx", 0.5)
+    .attr("cy", 0.5);
+    
   svg.append("g").attr("id", "d3-die")
 
   d3Wafermap.update(el, data)
@@ -27,20 +41,10 @@ d3Wafermap.update = (el, data) => {
     .attr("viewBox", [2 * min_x, 2 * min_y, vWidth, vHeight])
 
   const outer_c = svg.select("#d3-outer-circ")
-    .style("fill", "gray")
-    .style("stroke", "black")
-    .style("stroke-width", 0.025)
-    .attr("r", radius + 0.2)
-    .attr("cx", 0.5)
-    .attr("cy", 0.5);
+    .attr("r", radius + 0.2);
 
   const inner_c = svg.select("#d3-inner-circ")
-    .style("fill", "gray")
-    .style("stroke", "black")
-    .style("stroke-width", 0.025)
-    .attr("r", radius)
-    .attr("cx", 0.5)
-    .attr("cy", 0.5);
+    .attr("r", radius);
 
   const g = svg.select("#d3-die")
     .selectAll("rect")
