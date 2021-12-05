1436011a3eac7f1126b1f25cb2aa2d3436460256
Stiofain <stiofainf@gmail.com>
2021-12-05 22:58:17 +0000

    Add eslint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
+import './App.css'
+import data from './waferdata.json'
 import React, { useState } from 'react'
-import Wafermap from './components/Wafermap.js';
+import Wafermap from './components/Wafermap.js'
 
 function App() {
   const [waferIdx, setWaferIdx] = useState(0)
-
-  var data = require('./waferdata.json');
-  var dimensions = {"width": 400, "height": 400}
+  const dimensions = {'width': 400, 'height': 400}
 
   function nextWafer() {
     waferIdx === (data.length - 1) ?
@@ -17,12 +16,12 @@ function App() {
     <div className="App">
       <h3>Wafermap</h3>
       <Wafermap
-       points={data[waferIdx].points}
-       configuration={dimensions}
+        points={data[waferIdx].points}
+        configuration={dimensions}
       />
       <button onClick={nextWafer}>Next Wafer</button>
     </div>
   )
 }
 
-export default App;
+export default App
--- src/components/Wafermap.js
+++ src/components/Wafermap.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import d3Wafermap from './d3Wafermap.js';
+import React from 'react'
+import d3Wafermap from './d3Wafermap.js'
+import PropTypes from 'prop-types'
 
 class Wafermap extends React.Component {
-    componentDidMount() {
-        d3Wafermap.create(
-            this._rootNode,
-            this.props.points,
-            this.props.configuration
-        );
+  static get propTypes() { 
+    return { 
+      points: PropTypes.array, 
+      configuration: PropTypes.object
     }
+  }
+  
+  componentDidMount() {
+    d3Wafermap.create(
+      this._rootNode,
+      this.props.points,
+      this.props.configuration
+    )
+  }
 
-    componentDidUpdate(){
-        d3Wafermap.update(
-            this._rootNode,
-            this.props.points
-        );
-    }
+  componentDidUpdate(){
+    d3Wafermap.update(
+      this._rootNode,
+      this.props.points
+    )
+  }
 
-    _setRef(componentNode) {
-        this._rootNode = componentNode;
-    }
+  _setRef(componentNode) {
+    this._rootNode = componentNode
+  }
         
-    render() {
-        return (
-            <div
-            className="wafermap-container"
-            ref={this._setRef.bind(this)}
-            />
-        )
-    }
-};
+  render() {
+    return (
+      <div
+        className="wafermap-container"
+        ref={this._setRef.bind(this)}
+      />
+    )
+  }
+}
 
-export default Wafermap;
+export default Wafermap
